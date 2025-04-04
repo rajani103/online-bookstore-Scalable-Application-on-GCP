d7d901cd7b0c706d61bf1391788378fb42dff930
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2025-04-05 01:10:14 +0530

    Create db.js

---
+++ backend/db.js
@@ -0,0 +1,13 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: 5432,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
